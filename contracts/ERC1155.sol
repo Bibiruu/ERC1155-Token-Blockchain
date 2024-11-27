a3b3eda553d4df7053d81ac38d8fb6451ae91433
Maria <so.mariagloria@gmail.com>
2024-11-27 22:45:30 +0200

    functions

--- contracts/ERC1155.sol
+++ contracts/ERC1155.sol
@@ -2,8 +2,17 @@
 pragma solidity ^0.8.2;
 
 contract ERC1155 {
+
+
+     // TransferSingle()
+    // TransferBatch()
+    event ApprovalForAll(address indexed _owner, address indexed _operator, bool _approved);
+    // URI()
+
     //mapping from tokenID to accounts balances: input tokenId => user address, returning balance
     mapping(uint256 => mapping(address => uint256)) internal _balances;
+    //mapping from account to operator approvals. owner => operator => true or false
+    mapping(address => mapping(address => bool)) private _operatorApprovals;
 
     //gets the balance of accounts tokens
     function balanceOf(
@@ -18,21 +27,36 @@ contract ERC1155 {
     function balanceOfBatch(
         address[] memory accounts,
         uint256[] memory ids
-    ) public view returns (uint256) {
+    ) public view returns (uint256[] memory) {
+        require(accounts.length == ids.length, "Accounts and ids are not the same length");
         uint256[] memory batchBalances = new uint256[](accounts.length);
 
-        for (uint256 i = 0; i < accounts.length; i++) {
-            return batchBalances[i] = balanceOf(accounts[i], ids[i]);
+        for(uint256 i = 0; i < accounts.length; i++) {
+            batchBalances[i] = balanceOf(accounts[i], ids[i]);
         }
+        return batchBalances;
     }
 
-    // setApprovalForAll()
-    // isApprovedForAll()
-    // safeTransferFrom()
+    //enables or disables an operator to manage all of msg.senders assets
+    function setApprovalForAll(address operator, bool approved) public {
+        _operatorApprovals[msg.sender][operator] = approved;
+        emit ApprovalForAll(msg.sender, operator , approved);
+    }
+    //checks if an address is an operator or another address
+    function isApprovedForAll(address account, address operator) public view returns(bool) {
+        //checking if the operator is tru for the account
+        return _operatorApprovals[account][operator];
+    }
+
+    //
+    function _transfer(address from, address to, uint256 id, uint256 amount) private{
+        uint256 fromBalance = _balances[id][from];
+    }
+
+    //
+    function safeTransferFrom() {
+
+    }
     // safeBatchTransferFrom()
 
-    // TransferSingle()
-    // TransferBatch()
-    // ApprovalForAll()
-    // URI()
 }
